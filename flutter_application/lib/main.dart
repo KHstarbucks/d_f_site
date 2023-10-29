af97c9d032d459c825a452780f50e2ecbfc2a02f
seol <tjftmd7@gmail.com>
2023-10-29 23:56:11 +0900

    low bar added

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -11,7 +11,36 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         appBar: AppBar(
           title: const Text('메이플모아')
-      )
+      ),
+      drawer: MenuDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          switch (index){
+            case 0:
+              Navigator.pushNamed(context, '/');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/board');
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/union');
+              break;
+            case 3:
+              Navigator.pushNamed(context, '/calc');
+              break;
+            default:
+
+
+          }
+        },
+        items:[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label:'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.megaphone), label:'board'),
+          BottomNavigationBarItem(icon: Icon(Icons.union), label:'union'),
+          BottomNavigationBarItem(icon: Icon(Icons.calculate), label: 'calc')
+        ],
+
+      ),
       )
     )
   }
