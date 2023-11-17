63b9c8d3b0d35b18a6b2a0dea257364921ca6e58
seol <tjftmd7@gmail.com>
2023-11-17 20:52:51 +0900

    delete folder used flutterflow

--- community/community/README.md
+++
@@ -1,7 +0,0 @@
-# community
-
-A new Flutter project.
-
-## Getting Started
-
-FlutterFlow projects are built to run on the Flutter _stable_ release.
--- community/community/lib/index.dart
+++
@@ -1,5 +0,0 @@
-// Export pages
-export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/board/board_widget.dart' show BoardWidget;
-export '/union/union_widget.dart' show UnionWidget;
-export '/calculator/calculator_widget.dart' show CalculatorWidget;
