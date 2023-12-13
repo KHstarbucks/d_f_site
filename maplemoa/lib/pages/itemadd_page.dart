9c12459d5bef387ea4528baaeb7e0bafb57d1e34
seol <tjftmd7@gmail.com>
2023-12-13 23:17:22 +0900

    add imagebox

--- maplemoa/lib/pages/itemadd_page.dart
+++ maplemoa/lib/pages/itemadd_page.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:community/providers/palette.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class WritingPage extends StatefulWidget {
   const WritingPage({Key? key}) : super(key: key);
@@ -17,6 +18,8 @@ class _WritingPageState extends State<WritingPage> {
   final TextEditingController _contentController = TextEditingController();
   File? _selectedImage;
 
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
   Future<void> _getImage() async {
     final imagePicker = ImagePicker();
     final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
@@ -25,14 +28,57 @@ class _WritingPageState extends State<WritingPage> {
       _selectedImage = pickedFile != null ? File(pickedFile.path) : null;
     });
   }
+  void _addItem() async {
+    String title = _titleController.text.trim();
+    String content = _contentController.text.trim();
+    String author = currentUser?.displayName ?? 'Anonymous';
+
+    if(title.isNotEmpty && content.isNotEmpty){
+      if(_selectedImage != null){//사진 업로드 하는 경우
+        firebase_storage.Reference storageReference =
+          firebase_storage.FirebaseStorage.instance.ref().child('images/${DateTime.now()}.png');
+        firebase_storage.UploadTask uploadTask = storageReference.putFile(_selectedImage!);
+
+      await uploadTask.whenComplete(() async {
+        // 업로드 완료 후 이미지 URL을 가져와 Firestore에 저장
+        String imageUrl = await storageReference.getDownloadURL();
+
+        await FirebaseFirestore.instance.collection('items').add({
+          'title': title,
+          'content': content,
+          'author': author,
+          'createdAt': FieldValue.serverTimestamp(),
+          'itemPicture': imageUrl,
+        });
+      });
+
+      }else{//사진 업로드 안하는 경우
+        await FirebaseFirestore.instance.collection('items').add({
+          'title' : title,
+          'content' : content,
+          'author' : author,
+          'createdAt' : FieldValue.serverTimestamp(),
+      });
+      }
+      
+      _titleController.clear();
+      _contentController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('아이템 거래'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
         backgroundColor: Palette.mainColor,
-        
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -71,9 +117,13 @@ class _WritingPageState extends State<WritingPage> {
               color: Palette.deviderColor
             ),
             // 이미지 첨부 버튼 및 선택된 이미지 표시
-            ElevatedButton(
+            IconButton(
               onPressed: _getImage,
-              child: const Text('이미지 첨부'),
+              icon: const Icon(
+                Icons.collections,
+                size: 20
+              ),
+              alignment: Alignment.bottomLeft,
             ),
             if (_selectedImage != null) ...[
               const SizedBox(height: 8),
@@ -84,14 +134,15 @@ class _WritingPageState extends State<WritingPage> {
                 fit: BoxFit.cover,
               ),
             ],
-            const Spacer(),
             // 저장 버튼
             ElevatedButton(
               onPressed: () {
-                // 글 저장 로직 추가
-                // _titleController.text에 제목, _contentController.text에 내용
-                // _selectedImage에 선택된 이미지 파일
+                // 글 저장 로직
+                _addItem();
               },
+              style:ButtonStyle(
+                backgroundColor:MaterialStateProperty.all(Palette.mainColor),
+              ),
               child: const Text('저장'),
             ),
           ],
