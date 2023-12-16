3a6e945fc85a8451f2e8163ad3438dcb4c62703a
seol <tjftmd7@gmail.com>
2023-12-17 01:50:36 +0900

    delete no need files

--- maplemoa/lib/shared/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants{
-  static String appId = "1:925684634960:web:13d48e31c3d7949f984855";
-  static String apiKey = "AIzaSyCcSx6qLCPGxWldh7ZomPqSZ-Vy8dUvet8";
-  static String messagingSenderId ="925684634960";
-  static String projectId ="dtproject-3930e";
-}
